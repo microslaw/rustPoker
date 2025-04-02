8389b8411c691b0f6eaf4221919151eda8cda735
microslaw soter <milosz.grunwald@gmail.com>
2025-04-02 20:58:28 +0200

    add board and player structs

---
+++ rust-poker/src/game_types/player.rs
@@ -0,0 +1,12 @@
+use crate::card_tools::hand::*;
+
+pub struct Player {
+    pub hand: Hand,
+    money: u16,
+    bet: u16,
+    name: Box<str>,
+}
+
+impl Player {
+    //TODO
+}
--- rust-poker/src/main.rs
+++ rust-poker/src/main.rs
@@ -1,12 +1,12 @@
 mod card_tools;
-
+mod game_types;
 
 fn main() {
     let mut deck = card_tools::hand::get_sorted_deck();
 
     println!("{}", deck);
 
-    for _ in 0..7{
+    for _ in 0..47{
         let card = deck.pop_random();
         println!("{}", card);
     }
