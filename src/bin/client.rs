aa1c469e2a79f8d3b5cbfee2e11f2dedf1d05b16
microslaw <milosz.grunwald@gmail.com>
2025-04-25 17:09:20 +0200

    add deserialization example

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,6 +3,7 @@ use std::{
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
+use rust_poker::card_tools::card::Card;
 
 pub fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,7 +18,7 @@ pub fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
+    let json: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
@@ -27,5 +28,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write_all(response.as_bytes()).unwrap();
 
-    println!("Request: {http_request:#?}");
+    let c: Card = serde_json::from_str(&json.join("")).unwrap();
+
+    println!("Request: {c}");
 }
